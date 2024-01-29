3ef269cedc6675105902abd9c20e838a08143532
Nakiti <nikhilakiti91@gmail.com>
2024-01-29 11:02:10 -0800

    completed all migrations

--- migrations/2024-01-29-053305_enrollments/up.sql
+++ migrations/2024-01-29-053305_enrollments/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
-DROP TABLE enrollments (
+CREATE TABLE enrollments (
     enrollment_id SERIAL NOT NULL PRIMARY KEY,
     user_id VARCHAR NOT NULL references users(user_id),
     class_id SERIAL NOT NULL references classes(class_id),
